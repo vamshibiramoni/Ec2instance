600a4f4e7f86417792797721cdd150b00e45ffa6
vamshi biramoni <vamshi biramoni>
2017-03-25 22:05:41 -0400

    this is the first commit

--- src/main/java/awsproject/EC2instance.java
+++ src/main/java/awsproject/EC2instance.java
@@ -4,7 +4,7 @@ public class EC2instance {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(" This is simple Ec2 instance project");
+		System.out.println(" This is simple Ec2    instance project");
 	}
 
 }
